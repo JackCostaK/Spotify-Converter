ef54982b419656fd4bdc3e6db065184b2f59f5db
Jack <jackck1313@gmail.com>
2025-03-21 01:45:51 -0400

    Get URL from youtube search and download youtube videos to mp3

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import base64
 from requests import post, get
 import json
+import yt_dlp
+from youtube_search import YoutubeSearch
+
+
+
 
 load_dotenv()
 
@@ -46,13 +51,53 @@ def get_playlist_songs(token, playlist_id):
     json_result = json.loads(result.content)
 
     for i in range(len(json_result["tracks"]["items"])):
-        songs.append(f"{json_result["tracks"]["items"][i]["track"]["name"]} {json_result["tracks"]["items"][i]["track"]["artists"][0]["name"]}")
+        song_url = get_URL(f"{json_result["tracks"]["items"][i]["track"]["name"]} {json_result["tracks"]["items"][i]["track"]["artists"][0]["name"]}")
+        songs.append(song_url)
 
     return songs
+
+
+def get_URL(title):
+    
+
+    results = YoutubeSearch(title, max_results=1).to_dict()
+
+    return results[0]["url_suffix"]
+
+
+def download_song(url):
+    FFMPEG_LOCATION = "C:\\Users\\jackc\\AppData\\Local\\Programs\\Python\\Python313\\Lib\\site-packages\\ffmpeg-2025-03-20-git-76f09ab647-full_build\\bin\\ffmpeg.exe"
+
+    URL = "https://www.youtube.com" + url
+
+    ydl_opts = {
+
+        'ffmpeg_location':os.path.realpath(FFMPEG_LOCATION),
+        'format': 'bestaudio/best',
+
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(URL, download = True)
+
+    filename = ydl.prepare_filename(info_dict)
+    print(f"The downloaded file name is: {filename}")
 
 
 
 token = get_token()
 
-get_playlist_songs(token,"3nI5nwuS6RRFgbVKLtNbDN" )
+track_list = get_playlist_songs(token,"3nI5nwuS6RRFgbVKLtNbDN" )
+
+print(len(track_list))
+
+
+
+# for i in range(0, len(track_list)):
+#     download_song(track_list[i])
 
